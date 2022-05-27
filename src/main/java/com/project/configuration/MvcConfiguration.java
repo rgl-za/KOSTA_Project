a530d7bf0b5cef0f50bd4d6dea0f9c21c469a4ee
rgl-za <hyeonjeong2829@gmail.com>
2022-05-27 11:22:30 +0900

    [Feat] 개발 환경 설정

---
+++ src/main/java/com/project/configuration/MvcConfiguration.java
@@ -0,0 +1,18 @@
+package com.project.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.project.interceptor.LoggerInterceptor;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor())
+		.excludePathPatterns("/css/**", "/fonts/**", "/plugin/**", "/scripts/**");
+	}
+
+}
---
+++ src/main/java/com/project/interceptor/LoggerInterceptor.java
@@ -0,0 +1,29 @@
+package com.project.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoggerInterceptor implements HandlerInterceptor {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		logger.debug("===============================================");
+		logger.debug("==================== BEGIN ====================");
+		logger.debug("Request URI ===> " + request.getRequestURI());
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		logger.debug("==================== END ======================");
+		logger.debug("===============================================");
+	}
+
+}
