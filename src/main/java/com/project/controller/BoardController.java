2e8c87f98adcacc193df1b5f37f02d8f5f0b478f
rgl-za <hyeonjeong2829@gmail.com>
2022-05-31 17:22:26 +0900

    충돌 해결

--- src/main/java/com/project/controller/BoardController.java
+++ src/main/java/com/project/controller/BoardController.java
@@ -16,19 +16,10 @@ public class BoardController{
 
 	@GetMapping(value = "/")
 	public String openBoardList(Model model) {
-<<<<<<< HEAD
-=======
 		return "/index";
 		//return "/fragments/header";
 		//return "/layouts/index";
 		//return "/layouts/login";
-	}
->>>>>>> c20091d8f4bbaedfef8a8b5f7705dc7ddc4320b2
-
-//		return "/index";
-//		return "/fragments/header";
-//		return "/layouts/index";
-		return "/layouts/login";
 
 	}
 }
