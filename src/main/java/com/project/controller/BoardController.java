3a19b7966a74b5162868c485ea08cf3e97f76244
rgl-za <hyeonjeong2829@gmail.com>
2022-05-31 17:18:38 +0900

    no message

--- src/main/java/com/project/controller/BoardController.java
+++ src/main/java/com/project/controller/BoardController.java
@@ -4,23 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
-
 @Controller
 public class BoardController{
 
-
 	@GetMapping(value = "/")
 	public String openBoardList(Model model) {
-		System.out.println("ㅅㅂ");
-		return "/layouts/login";
-//		return "/layouts/index";
+		return "/layouts/blank";
 //		return "/layouts/register";
-		
 	}
-
-	
-	
-
 }
