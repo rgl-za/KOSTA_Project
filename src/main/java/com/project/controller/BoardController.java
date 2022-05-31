ad5a92b455baa67274d674eb1e50eb5b6827a1e7
jiwon83 <gkswldnj0803@gmail.com>
2022-05-31 13:58:13 +0900

    충돌해결

--- src/main/java/com/project/controller/BoardController.java
+++ src/main/java/com/project/controller/BoardController.java
@@ -16,7 +16,7 @@ public class BoardController{
 
 	@GetMapping(value = "/")
 	public String openBoardList(Model model) {
-		return "/index";
+		//return "/index";
 		//return "/fragments/header";
 		//return "/layouts/index";
 		//return "/layouts/login";
