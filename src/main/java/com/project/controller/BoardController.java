e6bc081edbf4311d4ee73e2581640cbce69f95e0
Mingyeong-Kim <65681568+Mingyeong-Kim@users.noreply.github.com>
2022-06-03 14:10:22 +0900

    타이머수정

--- src/main/java/com/project/controller/BoardController.java
+++ src/main/java/com/project/controller/BoardController.java
@@ -12,7 +12,7 @@ public class BoardController{
 
 	@GetMapping(value = "/")
 	public String openBoardList(Model model) {
-		return "/main";
+		return "/detail";
 		//return "/fragments/header";
 		//return "/layouts/index";
 		//return "/layouts/login";
