b6dbd19674f4e4b10a370202fc785885ca12a823
jiwon83 <gkswldnj0803@gmail.com>
2022-06-09 16:56:55 +0900

    충돌해결중

--- src/main/java/com/project/domain/PostDTO.java
+++ src/main/java/com/project/domain/PostDTO.java
@@ -12,47 +12,4 @@ public class PostDTO {
 	private String link;
 	private int price;
 
-	
-	public int getPnum() {
-		return pnum;
-	}
-	public void setPnum(int pnum) {
-		this.pnum = pnum;
-	}
-	public int getCatNum() {
-		return catNum;
-	}
-	public void setCatNum(int catNum) {
-		this.catNum = catNum;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getLink() {
-		return link;
-	}
-	public void setLink(String link) {
-		this.link = link;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
-	
-	
-	
-
-	
 }
