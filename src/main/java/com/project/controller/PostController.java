143c5cba7818bb0493ae7cb4c03fe840a05f11f6
jounghyunmun <monjh0613@gmail.com>
2022-06-10 10:00:32 +0900

    postcontroller + main.html
    쓰기는 오늘 시작하겠습니다

--- src/main/java/com/project/controller/PostController.java
+++ src/main/java/com/project/controller/PostController.java
@@ -20,23 +20,23 @@ public class PostController {
 	private PostService postService;
 
 	// 게시글 작성 폼으로
-	@GetMapping(value = "/post/write.do")
+	@GetMapping(value = "/write.do")
 	public String openPostWrite(@RequestParam(value = "pnum", required = false) Long pnum, Model model) {
 	  if (pnum == null) {
 	    model.addAttribute("post", new PostDTO());
 	  } else {
 	    PostDTO post = postService.getPostDetail(pnum);
 	    if (post == null) {
-	      return "redirect:/post/main.do";
+	      return "redirect:/main";
 	    }
 	    model.addAttribute("post", post);
 	  }
 
-	  return "post/write";
+	  return "/write";
 	}
 	
 	// 게시글 등록, 수정
-	@PostMapping(value = "/post/register.do")
+	@PostMapping(value = "/register.do")
 	public String registerPost(final PostDTO params) {
 	  try {
 	    boolean isRegistered = postService.registerPost(params);
@@ -49,16 +49,16 @@ public class PostController {
 	  } catch (Exception e) {
 	    // TODO => 시스템에 문제가 발생하였다는 메시지를 전달
 	  }
-	  return "redirect:/post/main.do";
+	  return "redirect:/main";
 	}
 	
 	// 게시글을 올리고 main으로
-	@GetMapping(value = "/post/main.do")
+	@GetMapping(value = "/main.do")
 	public String openPostList(Model model) {
 		List<PostDTO> postList = postService.getPostList();
 		model.addAttribute("postList", postList);
 		System.out.println("찍히나요?");
-		return "main";
+		return "/main";
 	}
 	
 	// 게시글 상세내용 detail
