8fc86b07216248fe896620dea4c59ea96ec73789
Mingyeong-Kim <65681568+Mingyeong-Kim@users.noreply.github.com>
2022-06-17 17:03:30 +0900

    main.html 타이머 기능 추가

--- src/main/java/com/project/controller/DealController.java
+++ src/main/java/com/project/controller/DealController.java
@@ -23,7 +23,7 @@ public class DealController {
 	private DealService dealService;
 	
 	
-	// 나의 거래완료 내역
+	// 나의 거래완료 내역(마이페이지)
 	@GetMapping(value = "/mydealhistory")
 	public String getEndDealList(Model model) {
 
@@ -33,10 +33,10 @@ public class DealController {
 		System.out.println("dealList" + dealList);
 		model.addAttribute("dealList", dealList);
 
-		return "/breakdown";
+		return "/mypage";
 	}
 	
-	// 나의 거래완료 내역
+	// 나의 거래중인 내역
 	@GetMapping(value = "/dealing")
 	public String getDealingList(Model model) {
 
