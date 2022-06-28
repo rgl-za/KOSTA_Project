8c1b684ea8f643073941be1e6667e45863b17dca
jiwon83 <gkswldnj0803@gmail.com>
2022-06-28 17:37:00 +0900

    편의점으로 중심변경 구현중, detail 수정
    - detail.html getNearstroe() x,y좌표 지정해서 검색하도록 변경
    - 두 지점 사이의 거리를 구하는 Dist2Point.class 생성
    - Map Controller에 근처 편의점 정보를 ajax통신하기 위해 생성-> 사용할지 모르겠음

--- src/main/java/com/project/controller/MapController.java
+++ src/main/java/com/project/controller/MapController.java
@@ -106,6 +106,71 @@ public class MapController {
 		return returnToAjax;
 	}
 
+	@RequestMapping(path = "/getNearCS2")
+	@ResponseBody
+	public Map<String, Object> getNearCS2(@RequestParam String data) throws ParseException, JSONException {
+
+		Map<String, Object> returnToAjax = new HashMap<String, Object>();
+
+		List<Point> pointList = new ArrayList<>();
+		/**** JSONArray 받는 방법 ****/
+		// request로 문자열형식의 데이터를 받고
+		String jsonStr = data;
+		System.out.println("string형태" + jsonStr);
+
+		JSONArray jsonArr = new JSONArray();
+		JSONParser parser = new JSONParser();
+
+		// 문자열 형식의 데이터를 JSONArray로 가공
+		jsonArr = (JSONArray) parser.parse(jsonStr);
+		System.out.println("JSONArray형태" + jsonArr);
+
+		for (int i = 0; i < jsonArr.size(); i++) {
+
+			// JSONArray->Map
+			Map<String, Object> getObj = (HashMap<String, Object>) jsonArr.get(i);
+
+			// List<Point>로 변환
+
+			// double idx = (long) getObj.get("idx");
+			// object -> double 한번에 변환 불가
+			String sx = (String) getObj.get("x");// object -> String
+			String sy = (String) getObj.get("y");// object -> String
+
+			double x = Double.parseDouble(sx);// String -> double
+			double y = Double.parseDouble(sy);// String -> double
+
+			Point userP = new Point(x, y);
+
+			pointList.add(userP);
+
+		}
+
+
+		Polygon poly = new Polygon();
+		// System.out.println("test출력결과"+poly.getPolygonMidPoint(pointList));
+		
+		long beforeTime1 = System.currentTimeMillis();
+		
+		returnToAjax.put("res", poly.getPolygonMidPoint(pointList));// point객체임
+		
+		long afterTime1 = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+		long secDiffTime1 = (afterTime1 - beforeTime1); //두 시간에 차 계산
+		System.out.println("3번 알고리즘 소요시간(m) : "+secDiffTime1);
+		
+		//알고리즘2 적용
+		long beforeTime2 = System.currentTimeMillis();
+		returnToAjax.put("two", poly.getPolyMid2(pointList));
+		
+		long afterTime2 = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+		long secDiffTime2 = (afterTime2 - beforeTime2); //두 시간에 차 계산
+		System.out.println("2번 알고리즘 소요시간(m) : "+secDiffTime2);
+
+		System.out.println("컨트롤러에서 ajax반환" + returnToAjax);
+
+		return returnToAjax;
+	}
+	
 	// post의 dealaddress변경
 	@RequestMapping(value = "/alterDealAdd.do")
 	@ResponseBody
