a02032a7ea19d7984b3373e06a6e032431405306
rgl-za <hyeonjeong2829@gmail.com>
2022-06-30 17:23:59 +0900

    추천 시스템 진행중...

--- src/main/java/com/project/recommend/ItemRecommend.java
+++ src/main/java/com/project/recommend/ItemRecommend.java
@@ -1,4 +1,16 @@
 package com.project.recommend;
 
 public class ItemRecommend {
+    public void itemSet(){
+        System.out.println("Apriori Algorithm TEST");
+        String[][] trans;
+        int[] freq={0};
+        int i=0;
+        for(int j=0; j< ; j++){
+            String word = trans[]
+            for(int k=0; k<trans.length; k++){
+
+            }
+        }
+    }
 }
