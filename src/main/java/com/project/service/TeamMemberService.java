c09ea5d7fa615227e90ed010f4bb723204be3067
Hanjione <gkswldnj0803@gmail.com>
2022-07-18 16:03:39 +0900

    detail 팀원 프로필 출력 수정
    전체 teammemberList -> 방장만 뺀 otherMemberList 추가

--- src/main/java/com/project/service/TeamMemberService.java
+++ src/main/java/com/project/service/TeamMemberService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface TeamMemberService {
     public List<UserDTO> getTeamMembertList(Long pnum);
+    public List<UserDTO> getOtherMembertList(Long pnum);
     public void registerTeamMember(TeamMemberDTO params);
     public int selectTeamMemberTotalCount(Long pnum);
     public int checkTeamMember(String userId, Long pnum);
