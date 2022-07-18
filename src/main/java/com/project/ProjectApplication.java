83f78b1023d2761362f889333cc3b60cb3c720ca
knh7532 <42673748+knh7532@users.noreply.github.com>
2022-07-18 17:27:52 +0900

    push8

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -1,6 +1,7 @@
 package com.project;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
